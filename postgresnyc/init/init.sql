c2bc5fff129520f3630bdedbfc47a0c91980ea96
Jorge Lambraño <jelambrar@gmail.com>
2024-08-14 18:46:53 +0000

    postgresnyc

---
+++ postgresnyc/init/init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS stagin_nycdatadl;
+CREATE TABLE stagin_nycdatadl(
+    filedatetime TIMESTAMP,
+    downloaddatetime TIMESTAMP,
+    typetrip VARCHAR,
+    pathfile VARCHAR,
+    bucket VARCHAR
+);
