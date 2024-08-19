bbd5138536127ffbad227a1c73b5b8548b0a44bb
Jorge Lambrano <jelambrar@gmail.com>
2024-08-19 09:48:21 -0500

    test_app.py

--- flask/test_app.py
+++ flask/test_app.py
@@ -46,10 +46,36 @@ class AppTestCase(unittest.TestCase):
         payload containing test data. It verifies that the response status 
         code is 200 (OK) and that the response JSON contains a 'prediction' key.
         """
-        test_data = {'feature1': 1, 'feature2': 2}
+        test_data = {
+            'data': [
+                {
+                    'VendorID': 2,
+                    'pickup_datetime': '2024-01-01 00:57:55',
+                    'dropoff_datetime': '2024-01-01 01:17:43',
+                    'passenger_count': 1.0,
+                    'trip_distance': 1.72,
+                    'RatecodeID': 1.0,
+                    'store_and_fwd_flag': 'N',
+                    'PULocationID': 186,
+                    'DOLocationID': 79,
+                    'payment_type': 2,
+                    'fare_amount': 17.7,
+                    'extra': 1.0,
+                    'mta_tax': 0.5,
+                    'tip_amount': 0.0,
+                    'tolls_amount': 0.0,
+                    'improvement_surcharge': 1.0,
+                    'total_amount': 22.7,
+                    'congestion_surcharge': 2.5,
+                    'Airport_fee': 0.0,
+                    'execution_date': '2024-01',
+                    'type_tripdata': 'yellow'
+                }
+            ]
+        }
         response = self.app.post('/predict', json=test_data)
         self.assertEqual(response.status_code, 200)
-        self.assertIn('prediction', response.get_json())
+        self.assertIn('predictions', response.get_json())
 
 
 if __name__ == '__main__':
